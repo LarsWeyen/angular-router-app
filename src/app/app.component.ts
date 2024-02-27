885f962e9301ee52a900194a11a1e9e54b62c8ca
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-02-27 10:37:00 +0100

    feat: add event to navigate to contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ContactModule } from './contact/contact.module';
+import { ProductsModule } from './products/products.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive,CommonModule,ContactModule, ProductsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-router-app';
+
+  constructor(private router: Router){}
+
+  goToContact(){
+    this.router.navigate(['contact']);
+  }
 }
